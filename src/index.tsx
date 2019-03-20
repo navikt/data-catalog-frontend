931b2aed192fc5830a0a749c8dfdce074548a0ef
Rituvesh <k153698@Rituveshs-MacBook-Pro.local>
2019-03-20 10:15:04 +0100

    Removed the un-used code.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.css';
-import './styles/sb-admin.css';
-import './styles/App.css';
+import './styles/main.css';
 // import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
