f5a0a77558a23dbf7a63ada50aabbc14d6618ee4
Rituvesh <rituvesh@gmail.com>
2019-03-27 14:18:49 +0100

    Continue adding some sample view for demo.

--- src/pages/consumers/Consumers.tsx
+++ src/pages/consumers/Consumers.tsx
@@ -7,7 +7,32 @@ class Consumers extends React.Component {
       <div>
         <h5>{I18n.t('dataCatalog.pages.consumers.consumers')}</h5>
         <hr style={{ height: '1px' }} />
-        <div>{I18n.t('dataCatalog.words.pageDetails')}</div>
+          <table className="table table-striped table-sm">
+              <thead>
+              <tr>
+                  <th>Application name</th>
+                  <th>Information type</th>
+                  <th>Purposes</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr key={1}>
+                  <td>Pesys</td>
+                  <td>Sivilstand</td>
+                  <td>AP, UP, BP, OP</td>
+              </tr>
+              <tr key={2}>
+                  <td>Pesys</td>
+                  <td>Fnr.</td>
+                  <td>AP, UP, BP, OP</td>
+              </tr>
+              <tr key={3}>
+                  <td>Pesys</td>
+                  <td>Navn</td>
+                  <td>AP, UP, BP, OP</td>
+              </tr>
+              </tbody>
+          </table>
       </div>
     );
   }
