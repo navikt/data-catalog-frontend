7a583223ca4f758be8a9dbec013a4a7b4ec5daa6
Rituvesh <rituvesh@gmail.com>
2019-05-06 10:14:26 +0200

    Added the button on the table component

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -7,6 +7,7 @@ import { Paginator } from './paginator/Paginator';
 import { sortColumn } from '../../common/pagination/sortingAndPagingActions';
 
 import './Table.css';
+import {I18n} from "react-i18nify";
 
 interface TableComponentProps {
   data: any;
@@ -50,6 +51,21 @@ const TableComponent = ({
       <div className="Table">
         <div className="Table-row header-row">
           {renderHeaders(children, previousQuery, onSortClick, !!collapseComponent)}
+          <div className="Table-header" >
+            <button
+                key="btn-add"
+                className="btn btn-primary"
+                disabled={false}
+                onClick={e => e}
+                title={
+                    1 === 1
+                        ? I18n.t('dataCatalog.words.doNotHaveSufficientRole')
+                        : I18n.t('dataCatalog.words.add')
+                }
+            >
+                {I18n.t('dataCatalog.words.add')}
+            </button>
+          </div>
         </div>
         <div className={tableBodyClassName}>
           {renderRows(data, idKey, collapseComponent, onToggleClick, children, isLoading)}
