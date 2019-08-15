645391bd8b276ac04cc35f494dc3d85d7d2d04dd
Andreas Skomedal <andreas.skomedal@nav.no>
2019-08-15 15:38:04 +0200

    get vault variables

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,3 +1,4 @@
+# At node build time REACT_APP_*** env variables should be set to the vault variable name (same key and value is simple)
 set -e
 for f in $(find /usr/share/nginx/html/static/js -name "*.js"); do
   for v in $(env | grep REACT_APP); do
