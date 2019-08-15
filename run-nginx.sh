aca14cfa00fb92864ffcc33cc028d9a5f3a866c1
Andreas Skomedal <andreas.skomedal@nav.no>
2019-08-15 14:58:14 +0200

    get vault variables

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,1 +1,2 @@
-nginx -g 'daemon off;'
+for f in $(find /usr/share/nginx/html/static/js -name "*.js"); do envsubst < $f > "/usr/share/nginx/html/static/js/$(basename $f)"; done && \
+  nginx -g 'daemon off;'
