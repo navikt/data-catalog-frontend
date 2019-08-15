174e9cfc758559c2892ab776b0e011a3e134d359
Andreas Skomedal <andreas.skomedal@nav.no>
2019-08-15 15:30:35 +0200

    get vault variables

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,2 +1,7 @@
-for f in $(find /usr/share/nginx/html/static/js -name "*.js"); do envsubst < $f > "/usr/share/nginx/html/static/js/$(basename $f)"; done && \
-  nginx -g 'daemon off;'
+set -e
+for f in $(find /usr/share/nginx/html/static/js -name "*.js"); do
+  for v in $(env | grep REACT_APP); do
+    sed -i "s|$(echo $v | cut -d'=' -f1)|$(echo $v | cut -d'=' -f2)|g" "/usr/share/nginx/html/static/js/$(basename $f)"
+  done
+done
+nginx -g 'daemon off;'
